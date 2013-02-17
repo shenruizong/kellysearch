da4903d64f5d569ed9b150328f5f1ca7673a6dd0
disappeared <shenruizong@gmail.com>
2013-02-18 01:05:31 +0800

    建立初步结构
    控制台命令。以及基本的功能

--- kellysearch/Program.cs
+++ kellysearch/Program.cs
@@ -9,7 +9,33 @@ namespace kellysearch
     {
         static void Main(string[] args)
         {
+            Program p = new Program();
+            Console.WriteLine("请输入网址");
+            string url = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("输入完毕,您输入的网址为"+url);
+            Console.WriteLine("请输入操作参数。");
+            Console.WriteLine("1、获取所有公司链接地址");
+            Console.WriteLine("2、获取公司传真");
+            string text = Console.ReadLine();
+            int i = int.Parse(text);
+            switch (i)
+            {
+                case 1:
+                    p.GetUrl(url);
+                    break;
+                case 2:
+                    p.GetFax();
+                    break;
+            }
+        }
+        private void GetUrl(string url)
+        {
 
+        }
+        private void GetFax()
+        {
+ 
         }
     }
 }
